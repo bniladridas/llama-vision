4d81ab2edca23a829f30e4ea9877acf3a6ad4def
Niladri Das <125604915+bniladridas@users.noreply.github.com>
2025-10-11 22:23:37 +0530

    fix: simplify cypress config without defineconfig

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,10 +1,5 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'http://localhost:3000',
   },
-})
+}
